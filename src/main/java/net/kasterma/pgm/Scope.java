a6bab5f97beea1c74f2511f1a44d0408d65819ec
Bart Kastermans <kasterma@kasterma.net>
2018-04-20 07:53:18 +0200

    Create scope helper functions

--- src/main/java/net/kasterma/pgm/Scope.java
+++ src/main/java/net/kasterma/pgm/Scope.java
@@ -30,10 +30,61 @@ public class Scope {
         List<Integer> strides = new ArrayList<>();
         int prod = 1;
         for (Domain d: domains) {
-            prod *= d.getSize();
             strides.add(prod);
+            prod *= d.getSize();
         }
         return strides;
+    }
+
+    /**
+     * Number of values needed in a factor that has this scope.
+     *
+     * @return size to reserve for the double[] val array.
+     */
+    int size() {
+        return domains.stream()
+                .map(Domain::getSize)
+                .reduce((i, j) -> i*j)
+                .orElse(0);
+    }
+
+    /**
+     * Compute index into the value array.
+     *
+     * @param idxs the indices in the different domains in the scope.
+     * @return the index into the double[] val array.
+     */
+    int index(final int[] idxs) {
+        List<Integer> strides = getStrides();
+        if (idxs.length != strides.size()) {
+            throw new IllegalArgumentException("not the correct number of idxs");
+        }
+        int idx = 0;
+        for (int i = 0; i < strides.size(); i++) {
+            idx += strides.get(i) * idxs[i];
+        }
+        return idx;
+    }
+
+    /**
+     * Compute the indices in the different domains.
+     *
+     * @param idx the index into the double[] val array.
+     * @return indices into the different domains.
+     */
+    int[] indices(final int idx) {
+        if (idx >= size()) {
+            throw new IllegalArgumentException("idx larger that scope allows");
+        }
+        List<Integer> strides = getStrides();
+        int[] idxs = new int[domains.size()];
+        int j = idx;
+        for (int i = strides.size() - 1; i >=0; i--) {
+            int stride = strides.get(i);
+            idxs[i] = j / stride;
+            j = j % stride;
+        }
+        return idxs;
     }
 }
 
--- src/test/java/net/kasterma/pgm/ScopeTest.java
+++ src/test/java/net/kasterma/pgm/ScopeTest.java
@@ -1,11 +1,16 @@
 package net.kasterma.pgm;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
+@Log4j2
 public class ScopeTest {
     @Test
     public void getStrides() {
@@ -17,8 +22,8 @@ public class ScopeTest {
         List<Integer> strides = s1.getStrides();
         assertEquals(strides.size(), 3);
         assertEquals((int) strides.get(0), 1);
-        assertEquals((int) strides.get(1), 2);
-        assertEquals((int) strides.get(2), 6);
+        assertEquals((int) strides.get(1), 1);
+        assertEquals((int) strides.get(2), 2);
 
         Scope s2 = Scope.builder()
                 .addDomain(0, 2)
@@ -27,8 +32,52 @@ public class ScopeTest {
                 .build();
         strides = s2.getStrides();
         assertEquals(strides.size(), 3);
-        assertEquals((int) strides.get(0), 2);
-        assertEquals((int) strides.get(1), 4);
-        assertEquals((int) strides.get(2), 12);
+        assertEquals((int) strides.get(0), 1);
+        assertEquals((int) strides.get(1), 2);
+        assertEquals((int) strides.get(2), 4);
+    }
+
+    @Test
+    public void indexOperations() {
+        Scope s1 = Scope.builder()
+                .addDomain(new Domain(0, 2))
+                .addDomain(new Domain(1, 2))
+                .addDomain(new Domain(2, 3))
+                .build();
+
+        for (int i = 0; i < s1.size(); i++) {
+            String idxsStr = Arrays.stream(s1.indices(i))
+                    .mapToObj(Integer::toString)
+                    .collect(Collectors.joining(", "));
+            log.info("idx {} idxs {}", i, idxsStr);
+        }
+
+        int[] idxs = {0, 0, 0};
+        assertEquals(0, s1.index(idxs));
+        idxs[0] = 1;
+        assertEquals(1, s1.index(idxs));
+        idxs[0] = 0;
+        idxs[1] = 1;
+        assertEquals(2, s1.index(idxs));
+        idxs[1] = 0;
+        idxs[2] = 2;
+        assertEquals(8, s1.index(idxs));
+        for (int i = 0; i < s1.size(); i++) {
+            assertEquals(i, s1.index(s1.indices(i)));
+        }
+        int[] idxs_in = new int[3];
+        IntStream.range(0, 2).forEach(i -> {
+            IntStream.range(0, 2).forEach(j -> {
+                IntStream.range(0, 3).forEach(k -> {
+                    idxs_in[0] = i;
+                    idxs_in[1] = j;
+                    idxs_in[2] = k;
+                    int[] idxs_out = s1.indices(s1.index(idxs_in));
+                    assertEquals(i, idxs_out[0]);
+                    assertEquals(j, idxs_out[1]);
+                    assertEquals(k, idxs_out[2]);
+                });
+            });
+        });
     }
 }
